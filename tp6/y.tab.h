40581bca21851802e1846f2d9e2460e427af76dc
Auxer1821 <brunojuansartori@hotmail.com>
2022-11-03 11:53:58 -0300

    tp de mierda

---
+++ tp6/y.tab.h
@@ -0,0 +1,112 @@
+
+/* A Bison parser, made by GNU Bison 2.4.1.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     MIENTRAS = 258,
+     SI = 259,
+     SINO = 260,
+     ESCRIBIR = 261,
+     VERACIDAD = 262,
+     ENTERO = 263,
+     PALABRA = 264,
+     INICIADOR = 265,
+     FIN = 266,
+     IDENTIFICADOR = 267,
+     MENORIGUAL = 268,
+     MENOR = 269,
+     MAYORIGUAL = 270,
+     MAYOR = 271,
+     SEMEJANTE = 272,
+     DIFERENTE = 273,
+     Y = 274,
+     O = 275,
+     SUMA = 276,
+     RESTA = 277,
+     MUTIPLICACION = 278,
+     DIVICION = 279,
+     POTENCIA = 280,
+     NUMERO = 281,
+     VERDADERO = 282,
+     FALSO = 283,
+     CADENA = 284
+   };
+#endif
+/* Tokens.  */
+#define MIENTRAS 258
+#define SI 259
+#define SINO 260
+#define ESCRIBIR 261
+#define VERACIDAD 262
+#define ENTERO 263
+#define PALABRA 264
+#define INICIADOR 265
+#define FIN 266
+#define IDENTIFICADOR 267
+#define MENORIGUAL 268
+#define MENOR 269
+#define MAYORIGUAL 270
+#define MAYOR 271
+#define SEMEJANTE 272
+#define DIFERENTE 273
+#define Y 274
+#define O 275
+#define SUMA 276
+#define RESTA 277
+#define MUTIPLICACION 278
+#define DIVICION 279
+#define POTENCIA 280
+#define NUMERO 281
+#define VERDADERO 282
+#define FALSO 283
+#define CADENA 284
+
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+extern YYSTYPE yylval;
+
+
